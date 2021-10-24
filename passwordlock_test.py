064fb8a2abac2f4d4ac1bda5ed0456ba376d0146
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 13:33:46 +0300

    It will create a  Test class that defines test cases for the User class

---
+++ passwordlock_test.py
@@ -0,0 +1,12 @@
+import unittest
+from passwordlock import User
+from passwordlock import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    It will create a  Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        It creates a method that runs before each individual test methods run.
+        """
