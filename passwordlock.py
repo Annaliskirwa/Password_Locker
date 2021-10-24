186725369904921b49ec4d05d5af2389efdbf5ef
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 09:05:44 +0300

    It will create a method that checks if a credential exists from the credential list and returns true or false

--- passwordlock.py
+++ passwordlock.py
@@ -83,3 +83,12 @@ def copy_password(cls, account):
     found_credentials = Credentials.find_credential(account)
     pyperclip.copy(found_credentials.password)
 
+@classmethod
+def if_credential_exist(cls, account):
+    """
+    It will create a method that checks if a credential exists from the credential list and returns true or false
+    """
+    for credential in cls.credentials_list:
+        if credential.account == account:
+            return True
+    return False
