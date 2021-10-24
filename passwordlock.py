f2b6a3cdb64deb5f074703b50fcc186db4b7d76b
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 08:19:38 +0300

    method that deletes a user from the list

--- passwordlock.py
+++ passwordlock.py
@@ -12,3 +12,20 @@ def __init__ (self, username, password):
     """
     It will create the method that defines properties of the user
     """
+    self.username = username
+    self.password = password
+
+def save_user(self):
+    """
+    It will create the method that saves a new usert into the users list
+    """
+    User.user_list.append(self)
+
+@classmethod
+def display_user(cls):
+    return cls.user_list
+
+def delete_user(self):
+    """
+    It will create the method that deletes a user from the list, deletes saved account
+    """
