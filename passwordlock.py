409435ec66929e392f3bd01ab32ec3431620434f
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 08:14:42 +0300

    method that defines properties of the user

--- passwordlock.py
+++ passwordlock.py
@@ -6,3 +6,9 @@ class User:
     """
     It will create the user class that generates instances of the user
     """
+user_list = []
+
+def __init__ (self, username, password):
+    """
+    It will create the method that defines properties of the user
+    """
