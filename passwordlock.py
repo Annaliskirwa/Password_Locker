9f533373c6ae08d2d0de3fc56f3f55158f3578af
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 12:17:20 +0300

    It will fix the userinterface errors

--- passwordlock.py
+++ passwordlock.py
@@ -1,109 +1,105 @@
-import pyperclip
-import string
 import random
-
+import string
+import pyperclip
 class User:
     """
-    It will create the user class that generates instances of the user
-    """
-user_list = []
+    Create User class that generates new instances of a user.
 
-def __init__ (self, username, password):
     """
-    It will create the method that defines properties of the user
-    """
-    self.username = username
-    self.password = password
+    user_list = []
 
-def save_user(self):
-    """
-    It will create the method that saves a new usert into the users list
-    """
-    User.user_list.append(self)
+    def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
 
-@classmethod
-def display_user(cls):
-    return cls.user_list
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
+    
 
-def delete_user(self):
-    """
-    It will create the method that deletes a user from the list, deletes saved account
-    """
-    User.user_list.remove(self)
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        '''
+        delete_account method deletes a  saved account from the list
+        '''
+        User.user_list.remove(self)
 
 class Credentials():
     """
-    It will create the credentials class to create a new object of the credentials
+    Create credentials class to help create new objects of credentials
     """
-credentials_list = []
-@classmethod
-def verify_user(cls, username, password):
-    """
-    It will create a method to verify if the user is in the user list
-    """
-    a_user = ""
-    for user in User.user_list:
-        if(user.username == username and user.password == password):
-            a_user == user.username
-    return a_user
+    credentials_list = []
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
 
-def __init__(self, account, userName, password):
-    """
-    It will create a method that defines user credentials to be stored
-    """
-    self.account = account
-    self.userName = userName
-    self.password = password
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password
+    
+    def save_details(self):
+        """
+        method to store a new credential to the credentials list
+        """
+        Credentials.credentials_list.append(self)
 
-def save_details(self):
-    """
-    It will create a method that stores a new credential to the credential list
-    """
-    Credentials.credential_list.append(self)
+    def delete_credentials(self):
+        """
+        delete_credentials method that deletes an account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
+    
+    @classmethod
+    def find_credential(cls, account):
+        """
+        Method that takes in a account_name and returns a credential that matches that account_name.
 
-def delete_credentials(self):
-    """
-    It will create a method that deletes credential from the credential list
-    """
-    Credentials.credential_list.remove(self)
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+    @classmethod
+    def copy_password(cls,account):
+        found_credentials = Credentials.find_credential(account)
+        pyperclip.copy(found_credentials.password)
 
-@classmethod
-def find_credentials(cls, account):
-    """
-    It will create a method that takes in a account_name and returns a credential that matches that account_name.
-    """
-    for credential in cls.credentials_list:
-        if credential.account == account:
-            return credential
-@classmethod
-def copy_password(cls, account):
-    """
-    It will create a method that uses pyperclip to copy the password
-    """
-    found_credentials = Credentials.find_credential(account)
-    pyperclip.copy(found_credentials.password)
+    @classmethod
+    def if_credential_exist(cls, account):
+        """
+        Method that checks if a credential exists from the credential list and returns true or false depending if the credential exists.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
+    @classmethod
+    def display_credentials(cls):
+        """
+        Method that returns all items in the credentials list
 
-@classmethod
-def if_credential_exist(cls, account):
-    """
-    It will create a method that checks if a credential exists from the credential list and returns true or false
-    """
-    for credential in cls.credentials_list:
-        if credential.account == account:
-            return True
-    return False
+        """
+        return cls.credentials_list
 
-@classmethod
-def display_credentials(cls):
-    """
-    It will create a method that returns all items in the credentials list
-    """
-    return cls.credentials_list
-
-def generatePassword(stringLength=8):
-    """
-    It will create a method that generates a random password string of letters and digits and special characters
-    """
-    password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
-    return "".join(random.choice(password) for i in range(stringLength))
-        
+    def generatePassword(stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
--- userinterface.py
+++ userinterface.py
@@ -1,82 +1,80 @@
 #!/usr/bin/env python3.6
 from passwordlock import User, Credentials
 
-def create_new_user(username, password):
-    """
-    It will create a function that creates a new user with a username and password
-    """
-    new_user = User(username, password)
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
     return new_user
 
 def save_user(user):
-    """
-    It will create a function that saves a new user
-    """
+    '''
+    Function to save a new user
+    '''
     user.save_user()
-
 def display_user():
     """
-    It will create a function that displays an existing user
+    Function to display existing user
     """
     return User.display_user()
-
-def login_user(username, password):
+def login_user(username,password):
     """
-    It will create a function that checks whether a user exist and then login the user in.
+    function that checks whether a user exist and then login the user in.
     """
-    check_user = Credentials.verify_user(username, password)
+  
+    check_user = Credentials.verify_user(username,password)
     return check_user
 
-def create_new_credential(account, userName, password):
+def create_new_credential(account,userName,password):
     """
-    It creates a function that creates new credentials for a given user account
+    Function that creates new credentials for a given user account
     """
-    new_credential = Credentials(account, userName, password)
+    new_credential = Credentials(account,userName,password)
     return new_credential
-
 def save_credentials(credentials):
     """
-    It creates a function that saves credentials to the credentials list
+    Function to save Credentials to the credentials list
     """
-    credentials.save_details()
-
-def display_account_details():
+    credentials. save_details()
+def display_accounts_details():
     """
-    It creates a function that returns all the saved credentials
+    Function that returns all the saved credential.
     """
     return Credentials.display_credentials()
 
 def delete_credential(credentials):
     """
-    It creates a function that deletes credential from the credential list
+    Function to delete a Credentials from credentials list
+
     """
     credentials.delete_credentials()
 
 def find_credential(account):
     """
-    It creates a function that finds a Credentials by an account name and returns the Credentials that belong to that account
+    Function that finds a Credentials by an account name and returns the Credentials that belong to that account
     """
     return Credentials.find_credential(account)
-
-def check_credentials(account):
+def check_credendtials(account):
     """
-    It creates a function that checks if a Credentials exists with that account name and returns true or false
+    Function that check if a Credentials exists with that account name and return true or false
+
     """
     return Credentials.if_credential_exist(account)
 
 def generate_Password():
-    """
-    It creates a function that generates a random password for the user
-    """
-    auto_password = Credentials.generatePassword()
+    '''
+    generates a random password for the user.
+    '''
+    auto_password=Credentials.generatePassword()
     return auto_password
-
 def copy_password(account):
     """
-    A function  that copies the password using the pyperclip framework
+    A funct that copies the password using the pyperclip framework
     We import the framework then declare a function that copies the emails.
     """
     return Credentials.copy_password(account)
+
 
 def passlocker():
     print("Hello Welcome to your Accounts Password Store...\n Please enter one of the following to proceed.\n CA ---  Create New Account  \n LI ---  Have An Account  \n")
