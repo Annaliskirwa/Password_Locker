c2b48fd673e0ff3689229ad2d37c5bdb9cf1718a
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-24 09:35:57 +0300

    It will create a function that creates a new user

--- userinterface.py
+++ userinterface.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3.6
 from passwordlock import User, Credentials
+
+def create_new_user(username, password):
+    """
+    It will create a function that creates a new user with a username and password
+    """
+    new_user = User(username, password)
+    return new_user
